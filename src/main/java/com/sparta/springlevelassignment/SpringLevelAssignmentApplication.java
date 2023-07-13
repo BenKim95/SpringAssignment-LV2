396fed3809444d708db8ee862ee9c124405d810f
BenKim95 <forseminaar@gmail.com>
2023-07-13 12:35:00 +0900

    init commit

---
+++ src/main/java/com/sparta/springlevelassignment/SpringLevelAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springlevelassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLevelAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLevelAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springlevelassignment/SpringLevelAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springlevelassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLevelAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
